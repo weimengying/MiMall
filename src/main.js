577d8d5eefcec024f67f13ad5fa8d930c5c7bea6
[weimengying] <[weimengying515@163.com]>
2020-09-29 13:53:20 +0800

    update fils

--- src/main.js
+++ src/main.js
@@ -10,19 +10,18 @@ Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 // 根据前端的跨域方式做调整 /a/b :/api/a/b=> /a/b
-axios.defaults.baseURL = 'http://mall-pre.springboot.cn';
-axios.defaults.timeout = 8000;
+axios.defaults.baseURL = 'http://mall-pre.springboot.cn'
+axios.defaults.timeout = 8000
 // 接口错误拦截
 axios.interceptors.response.use(function (response) {
-  let res = response.data
-  if (res.status == 0) { //成功
-    return res.data;
-  } else if (res.status == 10) { //未登录
+  const res = response.data
+  if (res.status === 0) { // 成功
+    return res.data
+  } else if (res.status === 10) { // 未登录
     window.location.href = '/#/login'
   } else {
     alert(res.msg)
   }
-
 })
 new Vue({
   router,
